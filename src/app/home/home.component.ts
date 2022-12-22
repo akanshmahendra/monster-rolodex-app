ca4982254900231eaa1451f2d3bbe7bd467ec64e
akansh-mahendra <akansh.mahendra@nagarro.com>
2022-12-22 23:40:59 +0530

    -Added text filter functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { takeWhile } from 'rxjs';
 import { User } from '../shared/user.model';
 import { UserService } from '../shared/user.service';
 
@@ -7,15 +9,31 @@ import { UserService } from '../shared/user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   usersData: User[] = [];
+  filteredUserData: User[] = [];
+  isComponentActive: boolean = true;
+  filterForm: FormGroup;
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.userService.fetchUserDetails();
-    this.userService.usersDetail.subscribe(d => this.usersData = d);
+    this.userService.usersDetail.pipe(takeWhile(() => this.isComponentActive)).subscribe(d => {
+      this.usersData = d;
+      this.filteredUserData = this.usersData;
+    });
+    this.filterForm = new FormGroup({
+      filterText: new FormControl('')
+    });
+    this.filterForm.get('filterText')?.valueChanges.pipe(takeWhile(() => this.isComponentActive)).subscribe(elVal => {
+      this.filteredUserData = this.usersData.filter(user => user.name.includes(elVal));
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.isComponentActive = false;
   }
 
 }
